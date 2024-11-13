c5021904be543f948b24c623233a84ce8edc7d97
mhson281 <mhson281@gmail.com>
2024-11-12 21:00:07 -0600

    fixed syntax error in mappings.lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,14 +3,14 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.api.nvim_set_keymap
-local options { noremap = true, silent = true}
+local options = { noremap = true, silent = true }
 
 
 -- Do not use this mapping
 --map("n", ";", ":", { desc = "CMD enter command mode" })
 -- map("i", "jk", "<ESC>")
 -- Map <Esc> to exit terminal mode
-map('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+map('t', '<Esc>', '<C-\\><C-n>', options)
 -- Vertical split and open file
 map('n', '<leader>sv', ':vsplit<CR>:Telescope find_files<CR>', options)
 
